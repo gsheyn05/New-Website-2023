d4ec2281ca04fb468960f93db25c1f797e748d9f
gregdevvv <gsheyn05@gmail.com>
2023-04-13 22:16:19 -0400

    Update header.tsx

--- src/Components/PartsofHomePage/header.tsx
+++ src/Components/PartsofHomePage/header.tsx
@@ -68,7 +68,7 @@ const Header = ({text}:any ) => {
                     </Link>
                     <div className=" mt-4 justify-center m-auto ">
                         
-                        <div className="flex-row flex place-content-center gap-2 m-auto ">
+                        <div className="flex-row flex gap-2 ">
                             <a href="https://www.linkedin.com/in/gregory-s-833a4719a/"  target="_blank" rel="noopener noreferrer" className="">
                                 <AiFillLinkedin size={30}  />
                             </a>
